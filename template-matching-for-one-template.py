5c7dec79e089e0b01bd06d813e464abc9a04b6ca
muhosevo <50045401+muhosevo@users.noreply.github.com>
2021-02-24 13:48:42 +0300

    Update template-matching-for-one-template.py

--- template-matching-for-one-template.py
+++ template-matching-for-one-template.py
@@ -46,3 +46,41 @@ class TemplateMatch:
 if __name__ == '__main__':
     app = TemplateMatch(cv2.imread("C:/Users/Muhammed/Desktop/OpenCV/Udemy/templates/template-152.jpeg",0)) # location of template image
     app.run()
+print(-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------)
+
+import os
+import cv2
+
+
+templates = cv2.imread("C:/Users/Muhammed/Desktop/OpenCV/Udemy/templates/template-152.jpeg",0)
+cam = cv2.VideoCapture(0)
+
+w = templates.shape[1]
+h = templates.shape[0]
+
+
+while True:
+    _, image = cam.read()
+    frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    result = cv2.matchTemplate(frame, templates, cv2.TM_CCOEFF_NORMED)
+
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    top_left = max_loc
+    
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    if max_val >= 0.6:
+        label = '{:.2f}%'.format( max_val * 100)
+
+        cv2.putText(image, label, (top_left[0], top_left[1] - 10), cv2.FONT_HERSHEY_COMPLEX, 0.5, (255, 255, 255), 1)
+        cv2.rectangle(image, top_left,bottom_right, (32, 32, 32), 2)
+
+    else:
+        print('{:.2f}%'.format(max_val * 100))
+
+    cv2.imshow('temp match', image)
+
+    if (cv2.waitKey(1) & 0xFF) == ord("q"):
+        break
+cam.release()
+cv2.destroyAllWindows()
